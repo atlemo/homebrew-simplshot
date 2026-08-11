5834ac7023087130a47f4e4d1a985ee120651762
Atle Mo <mo@atle.co>
2026-08-11 16:29:03 +0200

    Update simplshot.rb

--- Casks/simplshot.rb
+++ Casks/simplshot.rb
@@ -1,6 +1,6 @@
 cask "simplshot" do
-  version "1.6.7"
-  sha256 "adf768707e496c73a8d5d41a5b525d55839346eec689e930a0917341bb079c78"
+  version "1.7.0"
+  sha256 "b528245d8b37e7df6705c59e08c38adfa6768819a7b0b17300be46d6ee44809d"
 
   url "https://atle.co/simplshot/SimplShot-#{version}.zip",
       verified: "atle.co/simplshot/"
